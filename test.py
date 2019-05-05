09f767d9a1b6ad6b6e2f64cd2592aec603d79391
Light <xiadingjiang@fagougou.com>
2019-05-05 14:43:16 +0800

    remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-str=r'^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}$'
-str=r'^[a-zA-Z0-9_][^@]+@[^\.]+.*?[^@\.]$'
-if re.match(str,'1111helasdasdlo.world@163qq.com'):
-	print('OK')
-else:
-	print("not a mail")
-	
