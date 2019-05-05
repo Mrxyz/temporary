edf58e863460d99fe3f39be998a34e8fe1be53a5
Light <xiadingjiang@fagougou.com>
2019-05-05 14:38:44 +0800

    change email test function

---
+++ email.py
@@ -0,0 +1,28 @@
+import re
+
+
+exp_str1 = r'^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}$'
+exp_str2 = r'^[a-zA-Z0-9_][^@]+@[^\.]+.*?[^@\.]$'
+
+
+def reTest(regularExp, test_str):
+    if re.match(regularExp, test_str):
+        print('ok')
+    else:
+        print("failed")
+
+
+def is_valid_email(str):
+    re_email = re.compile(r'^([\w|.]+)@(\w+.com)$')
+    if re_email.match(str):
+        print('ok', re_email.match(str).groups())
+    else:
+        print('failed')
+
+
+if __name__ == '__main__':
+    reTest(exp_str1, 'bill.gates@microsoft.com')
+    reTest(exp_str2, 'bill.gates@microsoft.com')
+    is_valid_email('bill.gates@microsoft.com')
+
+
